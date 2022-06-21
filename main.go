4a92d7f4c03d879c8e19e2f261d493d7a787d3a1
FK <enochzzg@gmail.com>
2022-06-21 13:31:27 +0800

    shell

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ import (
 
 var conn *gorm.DB
 var err error
-var cookie = "SESSIONIDD2=8802842194392105947:1655789319916:228920; Path=/"
+var cookie = "SESSIONIDD2=1951181306867688391:1655789450449:235529; Path=/"
 
 func main() {
 
